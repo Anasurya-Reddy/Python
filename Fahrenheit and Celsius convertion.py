743fb56f868c6a72d82676428556f75ebac756d3
Anasurya-Reddy <166827620+Anasurya-Reddy@users.noreply.github.com>
2024-07-22 14:16:22 +0530

    Create Fahrenheit and Celsius convertion.py

---
+++ Fahrenheit and Celsius convertion.py
@@ -0,0 +1,6 @@
+#Fahrenheit to Celsius
+f = int(input('Enter the temperature in Fahrenheit: '))
+print('Temperature in Celsius',(5*(f-32)/9))
+#Celsius to Fahrenheit
+c = int(input('Enter the temperature in Celsius: '))
+print('Temperature in F',(9*c/5)+32)
