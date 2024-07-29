8d7945e2054dc77d85dd3107c38693eaca912a6d
Anasurya-Reddy <166827620+Anasurya-Reddy@users.noreply.github.com>
2024-07-29 14:59:36 +0530

    Create sum of 10 numbers using for and while loop.py

---
+++ sum of 10 numbers using for and while loop.py
@@ -0,0 +1,10 @@
+sum = 0
+for i in range(1,11):
+    sum = sum+i
+print('Sum of first 10 numbers using for loop is',sum)
+j=1
+total=0
+while j<=10:
+    total=total+j
+    j=j+1
+print('Sum of first 10 numbers using while loop is',total)
