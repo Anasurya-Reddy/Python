1354b70a82e93034e736fd4279956640b0e5c36d
Anasurya-Reddy <166827620+Anasurya-Reddy@users.noreply.github.com>
2024-09-23 16:42:34 +0530

    Delete simple intrest.py

--- simple intrest.py
+++
@@ -1,6 +0,0 @@
-p = int(input('Enter princple amount:'))
-t = int(input('Enter time in months:'))
-r = int(input('Enter rate of intrest:'))
-i = (p*t*r)/100
-print('Simple Intrest=',i)
-print('Total Amount=',p+i)
