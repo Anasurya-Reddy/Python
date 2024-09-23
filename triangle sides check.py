42614d1cad411d0f7f2fa5c14457efe61e2a8389
Anasurya-Reddy <166827620+Anasurya-Reddy@users.noreply.github.com>
2024-09-23 16:48:44 +0530

    Delete triangle sides check.py

--- triangle sides check.py
+++
@@ -1,7 +0,0 @@
-a = int(input('Enter first side of triangle:'))
-b = int(input('Enter second side of triangle:'))
-c = int(input('Enter third side of triangle:'))
-if a+b>c and b+c>a and c+a>b:
-    print('It is a Valid Triangle')
-else:
-    print('It is a Not Valid Triangle')
