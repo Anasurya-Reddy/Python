185a01b1cc49b45d484b9734be6b1e97ef9a5fd2
Anasurya-Reddy <166827620+Anasurya-Reddy@users.noreply.github.com>
2024-09-23 16:39:48 +0530

    Delete day of a week.py

--- day of a week.py
+++
@@ -1,6 +0,0 @@
-import datetime
-d = int(input('Enter the date:'))
-m = int(input('Enter the month:'))
-y = int(input('Enter the year:'))
-date=datetime.date(y,m,d)
-print(date.strftime('The weekday is %A'))
