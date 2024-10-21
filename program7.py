7111cda826ac68e6efed5ca4d92804f3203e7d15
Anasurya-Reddy <166827620+Anasurya-Reddy@users.noreply.github.com>
2024-10-21 16:50:48 +0530

    Delete program7.py

--- program7.py
+++
@@ -1,9 +0,0 @@
-def RevList(l):
-    rl = []
-    for i in l:
-        rl.insert(0,i)
-    return rl
-
-list = [1,2,3,4,5,6,7,8,9]
-print("Given List :", list)
-print("Reversed List is ",RevList(list))
